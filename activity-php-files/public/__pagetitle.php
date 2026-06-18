4eceed0fea3debc1fcdfe97b4718363b7affeac6
kkversile <kiran.phpfish@gmail.com>
2026-06-18 09:14:16 +0530

    Remove duplicated helper folders

--- activity-php-files/public/__pagetitle.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_module_title_name = breadcrumbTITLE($currentpage);
-
-if ((isset($_GET['route'])) && $_GET['route'] != '') :
-    $add_route = $_GET['route'];
-endif;
-$pageTITLE = $lang["__" . $_module_title_name . "title" . $add_route];
-
-echo $pageTITLE;
